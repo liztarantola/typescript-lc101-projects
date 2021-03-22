91a8871e6e52ee1b6f916129c2103833fe4dccd8
Elizabeth Tarantola <elizabethtarantola@Elizabeths-MacBook-Pro.local>
2021-03-22 18:19:40 -0500

    first comit in studio

---
+++ studio/Astronaut.ts
@@ -0,0 +1,10 @@
+import { Payload } from './Payload.ts';
+
+export class Astronaut {
+    name: string;
+    constructor()
+}
+function astronautName (astronaut: Astronaut): string {
+    return astronaut.name;
+}
+
